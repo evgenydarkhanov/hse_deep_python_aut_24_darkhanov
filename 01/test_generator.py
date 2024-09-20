5124fd14f70d6a1842ad6720f953b7ef847216e4
evgenydarkhanov <makaroch817@gmail.com>
2024-09-20 18:01:11 +0300

    fix test_generator

--- 01/test_generator.py
+++ 01/test_generator.py
@@ -5,7 +5,7 @@ from generator import read_text, check_line
 class GeneratorTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.filename = '/texts.txt'
+        self.filename = '01/texts.txt'
         self.wolf = ['волк']
         self.words = ['с', 'в', 'это', 'же']
 
